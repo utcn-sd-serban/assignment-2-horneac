d51230af191160ace7129bf2ec80ea47b7247ad3
Horneac Emanuel <horneac.emanuel@yahoo.com>
2019-05-06 16:25:12 +0300

    broken large question component into smaller ones

--- assignment-2/src/view/QuestionList.js
+++ assignment-2/src/view/QuestionList.js
@@ -1,4 +1,6 @@
 import React from "react"
+import QuestionSearchBar from "./question/QuestionSearchBar";
+import QuestionButtons from "./question/QuestionButtons";
 
 const QuestionList = ({questions,onSearchTitle, newQuestion, 
     onChangeNewQuestionProperty, onLogOut,
@@ -7,62 +9,22 @@ const QuestionList = ({questions,onSearchTitle, newQuestion,
         {/* <!-- Main container --> */}
         <nav className="level">
         {/* <!-- Left side --> */}
-        <div className="level-left">
-            <div className="level-item">
-            <p className="subtitle is-5">
-                <strong> </strong> questions
-            </p>
-            </div>
-            <div className="level-item">
-            <div className="field has-addons">
-                <p className="control">
-                <input className="input" value={tagOrTitle} onChange={e => onSearchBarChange(e.target.value)}/>
-                </p>
-                <p className="control">
-                <button className="button" onClick={onSearchTitle}>
-                    Search title
-                </button>
-                <button className="button" onClick={onSearchTag}>
-                    Search tag
-                </button>
-                </p>
-            </div>
-            </div>
-        </div>
+        <QuestionSearchBar onSearchTitle={onSearchTitle}
+                           onSearchTag={onSearchTag}
+                           tagOrTitle={tagOrTitle}
+                           onSearchBarChange={onSearchBarChange}/>
+       
 
         {/* <!-- Right side --> */}
-        <div className="level-right">
-            <div className="dropdown is-hoverable">
-                    <div className="dropdown-trigger">
-                        <button className="button is-success" aria-haspopup="true" aria-controls="dropdown-menu">
-                        <span>New</span>
-                        <span className="icon is-small">
-                            <i className="fas fa-angle-down" aria-hidden="true"></i>
-                        </span>
-                        </button>
-                    </div>
-                    <div className="dropdown-menu" id="dropdown-menu" role="menu">
-                        <div className="dropdown-content" allign="left">
-                        <label >Title</label>
-                        <input value={newQuestion.title} onChange={e => onChangeNewQuestionProperty("title", e.target.value)}></input>
-                        <label>Text</label>
-                        <textarea className="textarea" value={newQuestion.text} onChange={e => onChangeNewQuestionProperty("text", e.target.value)}/>
-                        <label>Tags</label>
-                        <input  type="tags" value={newQuestion.tags} onChange={e => onChangeNewQuestionProperty("tags", e.target.value)} ></input>
-                        <hr className="dropdown-divider"/>
-                        <button className="button" onClick={onClickNewQuestion}> Post</button>
-                        </div>
-                    </div>
-            </div>
-           
-            <p className="level-item"> <button className="button" onClick={onLogOut}>     Log out</button></p>
-            <p calssName="level-item"> <span/><span/></p>
-            
+        <QuestionButtons    newQuestion={newQuestion} 
+                            onChangeNewQuestionProperty={onChangeNewQuestionProperty}
+                            onLogOut={onLogOut}
+                            onClickNewQuestion={onClickNewQuestion} />
+        
             {/* <div className="dropdown">
                 <p className="level-item"><button className="button is-success" onClick={onClickNewQuestion}>New</button></p>
             </div> */}
             
-        </div>
         </nav>
         <h2 className="title primary"> Questions </h2>
         <div className="tile is-ancestor">
